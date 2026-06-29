22ec74b1f4d773d9b54c252964354c6523ebc6f4
nhocamingao20 <nhoviecamingao25@gmail.com>
2026-06-28 23:08:47 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function calculateSum(){
+    const num1 =document.getElementById('num1').value;
+    const num2 =document.getElementById('num2').value;
+
+    const number1 = parseFloat(num1);
+    const number2 = parseFloat(num2);
+
+    //calculate Sum
+    const sum =number1 + number2;
+
+    //display result
+    document.getElementById('result').textContent='The sum is ' +sum
+
+}
